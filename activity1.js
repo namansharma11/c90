4385c5e014d9296207aaf537b916aa3f412e1cd0
namansharma11 <78791879+namansharma11@users.noreply.github.com>
2021-09-16 18:55:54 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,13 @@
+var score =0
+function updateScore()
+{
+    score = score + 1;
+}
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+function nextPage()
+{
+    window. location = "activity_2.html";
+}
